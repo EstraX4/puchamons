d20fa4e88936b9c57c6bae6a39c7850129cce3ed
Juan David Estrada Carvajal <111063919+EstraX4@users.noreply.github.com>
2023-03-05 22:09:47 -0500

    Update Inicio.jsx

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function Inicio() {
   return (
-    <div>Inicio</div>
-  )
+    <div>
+      <h1 className="incio-text">
+        Esta parte esta pendiente porque soy un vago, si quieres que la
+        completemos dimelo en mi canal EstraX4, si, autopromocion ninja :D
+      </h1>
+    </div>
+  );
 }
