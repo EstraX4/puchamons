a563d5b9d394e2cf660b5b8b9d3f7047e249808b
Juan David Estrada Carvajal <111063919+EstraX4@users.noreply.github.com>
2023-03-09 17:40:46 -0500

    Enlace

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -5,7 +5,7 @@ export default function Inicio() {
     <div>
       <h1 className="incio-text">
         Esta parte esta pendiente porque soy un vago, si quieres que la
-        completemos dimelo en mi canal EstraX4, si, autopromocion ninja :D
+        completemos dimelo en mi canal <a className="ESTRAX" href="https://www.youtube.com/channel/UCVTT8CyI7sw8lNjBYrWdnUg" target="_blank" rel="noopener noreferrer">EstraX4</a>, si, autopromocion ninja :D
       </h1>
     </div>
   );
